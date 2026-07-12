e4941cc74aad8c4a072baf0eee7bfad694f732d1
sumedhaholkar <holkarsumedha@gmail.com>
2026-07-12 15:47:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for (char ch:s){
+            if(ch=='(' || ch=='{' || ch=='[')
+            st.push(ch);
+            else{
+                if(st.empty()) return false;
+                char tops=st.top();
+                st.pop();
+                if(ch==')' || tops!='(') return false;
+                if(ch==']' || tops!='[') return false;
+                if(ch=='}' || tops!='{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
