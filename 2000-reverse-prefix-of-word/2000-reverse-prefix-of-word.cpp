29d2775a185b9c4a023a0a5847ea2500b0a7cfca
sumedhaholkar <holkarsumedha@gmail.com>
2026-07-27 15:19:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (68.43%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index=-1;
+        for(int i=0;i<word.length();i++){
+            if(word[i]==ch){
+            index=i;
+            break;}
+        }
+        reverse(word.begin(),word.begin()+index+1);
+        return word;
+    }
+};
